2c781b04984a91eb67e0f31a9c8902809f4d4bcf
itaydafna <itaydafna@gmail.com>
2016-11-01 18:11:51 +0200

    created FilterPosts component

--- app/components/sidebar/sidebar.js
+++ app/components/sidebar/sidebar.js
@@ -1,77 +1,11 @@
 import {Component} from 'react'
 import {Search} from './search'
+import {FilterPosts} from './filter-posts'
 export const Sidebar = () =>(
     <aside className="col-md-4">
     {/* Blog Search Well */}
     <Search />
     {/* Blog Categories Well */}
-    <div className="well">
-        <h3>Filter Posts</h3>
-        <div className="list-group">
-            <a href="#" className="list-group-item active">
-                <span className="badge">8</span>
-                Show All Posts
-            </a>
-        </div>
-        <h4><small className="glyphicon glyphicon-tag" /> Category</h4>
-        <div className="list-group">
-            <a href="#" className="list-group-item">
-                <span className="badge">4</span>
-                AngularJS
-            </a>
-            <a href="#" className="list-group-item">
-                <span className="badge">2</span>
-                Grunt
-            </a>
-            <a href="#" className="list-group-item">
-                <span className="badge">6</span>
-                JavaScript
-            </a>
-            <a href="#" className="list-group-item">
-                <span className="badge">2</span>
-                jQuery
-            </a>
-            <a href="#" className="list-group-item">
-                <span className="badge">2</span>
-                Tools
-            </a>
-        </div>
-        <h4><small className="glyphicon glyphicon-user" /> Author</h4>
-        <div className="list-group">
-            <a href="#" className="list-group-item">
-                <span className="badge">2</span>
-                Alex Ilyaev
-            </a>
-            <a href="#" className="list-group-item">
-                <span className="badge">2</span>
-                Amit Choukroun
-            </a>
-            <a href="#" className="list-group-item">
-                <span className="badge">4</span>
-                Ilan Cohen
-            </a>
-        </div>
-        <h4><small className="glyphicon glyphicon-time" /> Month</h4>
-        <div className="list-group">
-            <div>
-              <span className="list-group-item disabled">
-                2015
-              </span>
-                <a href="#" className="list-group-item">
-                    <span className="badge">4</span>
-                    January
-                </a>
-            </div>
-            <div>
-              <span className="list-group-item disabled">
-                2014
-              </span>
-                <a href="#" className="list-group-item">
-                    <span className="badge">4</span>
-                    December
-                </a>
-            </div>
-        </div>
-    </div>
+    <FilterPosts />
 </aside>
 )
