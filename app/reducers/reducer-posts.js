af11feba2dee5b9a4f6fb299c3f01599ec305cc7
itaydafna <itaydafna@gmail.com>
2016-11-13 20:55:30 +0200

    got rid of get visible previews  reducer

--- app/reducers/reducer-posts.js
+++ app/reducers/reducer-posts.js
@@ -1,8 +1,4 @@
 const posts = (state = [], action) => {
-    // switch (action.type){
-    //     case SHOW_OLDER_POSTS:
-    //         visiblePreviews(undefined, showOlderPosts(state));
-    // }
     return state;
 };
 
