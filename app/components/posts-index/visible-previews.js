6e764897328e03bc604b0c56b3a06df7309e9d40
itaydafna <itaydafna@gmail.com>
2016-11-14 23:06:57 +0200

    wraped the switch block which filters the posts on the VisiblePreviews component so I can later turn it into a Selector on the main 'posts' reducer

--- app/components/posts-index/visible-previews.js
+++ app/components/posts-index/visible-previews.js
@@ -9,57 +9,62 @@ import {normalizeAuthor, normalizeTag, normalizeMonth} from '../../assets/UTILS'
 
 
 let VisiblePreviews = ({location, params, posts}) => {
+
+    //function which filters the posts array based on the 'filter-term' on the query param
+    let filterPostsArray = (posts,filterTerm,queryVar) => {
+        switch (queryVar) {
+            case 'author':
+                return [...posts.filter((post)=> {
+                    if (normalizeAuthor(post.author) === normalizeAuthor(filterTerm)) {
+                        return post
+                    }
+                })];
+                break;
+            case 'category':
+                return [...posts.filter((post)=> {
+                    if (post.tags.some((tag)=>(normalizeTag(tag) === normalizeTag(filterTerm)))) {
+                        return post;
+                    }
+                })];
+                break;
+            case 'month':
+                return [...posts.filter((post)=> {
+                    if (normalizeMonth(post.date) === filterTerm) {
+                        return post;
+                    }
+                })];
+                break;
+            case 'search':
+               return [...posts.filter((post)=> {
+                    if (
+                        //testing if the post's author name includes part of the filter term
+                    _.includes(normalizeAuthor(post.author), normalizeAuthor(filterTerm)) ||
+                    //testing if one or more(some) of the post's tags includes part of the filter term
+                    post.tags.some((tag)=>_.includes(normalizeTag(tag), normalizeTag(filterTerm))) ||
+                    //testing if the post's dates includes part of the filter term
+                    _.includes(normalizeMonth(post.date), filterTerm) ||
+                    //testing if the post's descrisption includes part of the filter term
+                    _.includes(post.description.toLowerCase(), filterTerm)
+
+                    ) {
+                        return post
+                    }
+                })];
+                break;
+
+            default:
+                return posts;
+        }
+    };
+
     //extracting the query var from the url
     let queryVar = Object.keys(location.query)[0];
     //extracting the filter term (query val) from the url
     let filterTerm = location.query[queryVar] || '';
 
-
     //filtering the posts array based on the 'filter-term' on the query param
-    let filteredPostsArray = [];
-    switch (queryVar) {
-        case 'author':
-            filteredPostsArray = posts.filter((post)=> {
-                if (normalizeAuthor(post.author) === normalizeAuthor(filterTerm)) {
-                    return post
-                }
-            });
-            break;
-        case 'category':
-            filteredPostsArray = posts.filter((post)=> {
-                if (post.tags.some((tag)=>(normalizeTag(tag) === normalizeTag(filterTerm)))) {
-                    return post;
-                }
-            });
-            break;
-        case 'month':
-            filteredPostsArray = posts.filter((post)=> {
-                if (normalizeMonth(post.date) === filterTerm) {
-                    return post;
-                }
-            });
-            break;
-        case 'search':
-            filteredPostsArray = posts.filter((post)=> {
-                if (
-                    //testing if the post's author name includes part of the filter term
-                _.includes(normalizeAuthor(post.author), normalizeAuthor(filterTerm)) ||
-                //testing if one or more(some) of the post's tags includes part of the filter term
-                post.tags.some((tag)=>_.includes(normalizeTag(tag), normalizeTag(filterTerm))) ||
-                //testing if the post's dates includes part of the filter term
-                _.includes(normalizeMonth(post.date), filterTerm) ||
-                //testing if the post's description includes part of the filter term
-                _.includes(post.description.toLowerCase(), filterTerm)
+    let filteredPostsArray = filterPostsArray(posts,filterTerm,queryVar);
 
-                ) {
-                    return post
-                }
-            });
-            break;
-
-        default:
-            filteredPostsArray = posts;
-    }
 
     //chunking the filtered posts array to pages
     let chunkedArray = _.chunk(filteredPostsArray, 3);
