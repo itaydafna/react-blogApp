099c81aed591685a13212eb90bc4b5f7628fcc6b
itaydafna <itaydafna@gmail.com>
2016-11-18 19:42:23 +0200

    got edit post to render on the correct route on tr click

--- app/components/admin/posts-table/post-table-row.js
+++ app/components/admin/posts-table/post-table-row.js
@@ -1,12 +1,24 @@
-import {Component} from 'react';
+import {Component, PropTypes} from 'react';
+import {Link} from 'react-router'
 
-export const PostTableRow = ({number,title,author,date})=> {
-    return (
-        <tr>
-            <th scope="row">{number}</th>
-            <td>{title}</td>
-            <td>{author}</td>
-            <td>{date}</td>
-        </tr>   
-    )
+import {removeNonLetters} from '../../../assets/UTILS'
+
+
+export class PostTableRow extends Component{
+    render() {
+        const {number, title, author, date} = this.props;
+        return (
+            <tr
+                onClick={()=>this.context.router.push(`admin/edit/post/${removeNonLetters(title)}`)}>
+                <th scope="row">{number}</th>
+                <td>{title}</td>
+                <td>{author}</td>
+                <td>{date}</td>
+            </tr>
+        )
+    }
 }
+
+PostTableRow.contextTypes = {
+    router: PropTypes.object
+};
