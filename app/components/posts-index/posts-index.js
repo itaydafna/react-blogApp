0aaabb3a4334e474c6fde8a6be27f4d4b5ce71ec
itaydafna <itaydafna@gmail.com>
2016-11-19 18:47:58 +0200

    posts array now getting sorted by date (descending) when PostsIndex comp is being rendered

--- app/components/posts-index/posts-index.js
+++ app/components/posts-index/posts-index.js
@@ -1,32 +1,52 @@
 import {Component} from 'react';
+import {connect} from 'react-redux'
+
 import {Sidebar} from '../sidebar/sidebar';
 
 
-const PostsIndex = ({
-    params,
-    children,
-    location
-}) => {
-    let currentPage = isNaN(Number(params.page)) ? 1 : Number(params.page),
-        queryVar = Object.keys(location.query) ? Object.keys(location.query)[0] : null,
-        queryVal = location.query[queryVar] || null;
+import {sortDescending, sortAscending} from '../../action-creators/sort-posts'
 
-    return (
-        <div>
-            <section className="col-md-8">
-                {/* the children components are
-                 is dynamic VisiblePreviews component  */}
-                {children}
-                {/*<VisiblePreviews />*/}
-            </section>
-            <Sidebar
-                filterTerm = {queryVal}
-                pathPrefix = {'/posts'}
-            />
-        </div>
-    )
 
-};
+class PostsIndex extends Component {
+
+   //sorting posts by Descending date order while PostIndex component is being rendered
+    componentWillMount(){
+        this.props.sortDescending('date');
+    }
+
+    render() {
+        const {params, children, location} = this.props;
+
+        let currentPage = isNaN(Number(params.page)) ? 1 : Number(params.page),
+            queryVar = Object.keys(location.query) ? Object.keys(location.query)[0] : null,
+            queryVal = location.query[queryVar] || null;
+
+
+
+        return (
+            <div>
+                <section className="col-md-8">
+                    {/* the children components are
+                     is dynamic VisiblePreviews component  */}
+                    {children}
+                    {/*<VisiblePreviews />*/}
+                </section>
+                <Sidebar
+                    filterTerm={queryVal}
+                    pathPrefix={'/posts'}
+                />
+            </div>
+        )
+    }
+}
+;
+
+PostsIndex = connect(
+    null,
+    {
+        sortDescending
+    }
+)(PostsIndex);
 
 
 export default PostsIndex;
